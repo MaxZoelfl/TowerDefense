4438e933cfc488c4343948628d9294204c4e413d
leonltnr <leonleitner@icloud.com>
2026-05-07 10:04:33 +0200

    TODO LIST

---
+++ PROJEKT/Controls.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
+
+public class Controls extends Actor
+{
+    /**
+     * Act - tut, was auch immer Controls tun will. Diese Methode wird aufgerufen, 
+     * sobald der 'Act' oder 'Run' Button in der Umgebung angeklickt werden. 
+     */
+    public void act() 
+    {
+        // Ergänzen Sie Ihren Quelltext hier...
+    }    
+}
