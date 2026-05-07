0dcaf39c1c40276cd9ed7d96b558fd712fe7a5c9
leonltnr <leonleitner@icloud.com>
2026-05-07 11:18:30 +0200

    BUTTON()

--- PROJEKT/Button.java
+++ PROJEKT/Button.java
@@ -1,19 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
 
-/**
- * Ergänzen Sie hier eine Beschreibung für die Klasse Button.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class Button extends Actor
 {
-    /**
-     * Act - tut, was auch immer Button tun will. Diese Methode wird aufgerufen, 
-     * sobald der 'Act' oder 'Run' Button in der Umgebung angeklickt werden. 
-     */
-    public void act() 
+    protected int buttonType;
+    
+    public Button (int buttonType) {
+        this.buttonType = buttonType;
+    }
+    
+    public void act () 
     {
-        // Ergänzen Sie Ihren Quelltext hier...
+        if (Greenfoot.mouseClicked(this)) {
+            //
+        }
     }    
 }
